b9fac617e02605f5c551d17b1d0277a8197ad9f0
BrA-ver <sambobraver041@gmail.com>
2025-08-29 11:14:40 +0200

    Made clues interactable

--- Assets/Merging Scripts/Player/Interaction/Interactor.cs
+++ Assets/Merging Scripts/Player/Interaction/Interactor.cs
@@ -5,53 +5,94 @@ using System;
 
 public class Interactor : MonoBehaviour
 {
-    [SerializeField] Vector3 checkOffset;
-    [SerializeField] float checkRadius;
-
-    List<Interactable> interactables = new List<Interactable>();
-
+    [SerializeField] float checkDistance = 3f;
+    [SerializeField] LayerMask interactbleLayer;
     Interactable interactable;
 
     private void Update()
     {
-        if (interactables.Count > 0f)
+        // Do a raycast to find interactables
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, checkDistance))
         {
-            interactable = interactables[0];
+            Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+            if (newInteractable == null) 
+            {
+                if (interactable)
+                    interactable.StopHighlight();
+                return; 
+            }
+
+            if (interactable == null)
+            {
+                interactable = newInteractable;
+            }
+            else if (interactable != newInteractable)
+            {
+                interactable.StopHighlight();
+                interactable = newInteractable;
+            }
+            
+            interactable.Highlight();
+            Debug.DrawLine(ray.origin, hit.point, Color.blue);
         }
         else
+        {
+            if (interactable)
+                interactable.StopHighlight();
             interactable = null;
-    }
-
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            Interact();
         }
     }
 
-    private void Interact()
-    {
-        if (!interactable) return;
+    ////[SerializeField] Vector3 checkOffset;
+    ////[SerializeField] float checkRadius;
 
-        interactable.Interact();
-    }
+    //List<Interactable> interactables = new List<Interactable>();
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.TryGetComponent(out Interactable interactable))
-        {
-            this.interactables.Add(interactable);
-            interactable.Highlight();
-        }
-    }
+    ////Interactable interactable;
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out Interactable interactable))
-        {
-            this.interactables.Remove(interactable);
-            interactable.StopHighlight();
-        }
-    }
+    ////private void Update()
+    ////{
+    ////    if (interactables.Count > 0f)
+    ////    {
+    ////        interactable = interactables[0];
+    ////    }
+    ////    else
+    ////        interactable = null;
+    ////}
+
+    ////public void OnInteract(InputAction.CallbackContext context)
+    ////{
+    ////    if (context.started)
+    ////    {
+    ////        Interact();
+    ////    }
+    ////}
+
+    ////private void Interact()
+    ////{
+    ////    if (!interactable) return;
+
+    ////    interactable.Interact();
+    ////}
+
+    ////private void OnTriggerEnter(Collider other)
+    ////{
+    ////    if (other.TryGetComponent(out Interactable interactable))
+    ////    {
+    ////        this.interactables.Add(interactable);
+    ////        interactable.Highlight();
+    ////    }
+    ////}
+
+    ////private void OnTriggerExit(Collider other)
+    ////{
+    ////    if (other.TryGetComponent(out Interactable interactable))
+    ////    {
+    ////        this.interactables.Remove(interactable);
+    ////        interactable.StopHighlight();
+    ////    }
+    ////}
 }
